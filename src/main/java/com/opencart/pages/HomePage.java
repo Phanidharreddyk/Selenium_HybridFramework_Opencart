f5ee7f6578f488bdfa38be87687ac9e2820410ed
Phanidharreddyk <kotalaphanidharreddy@gmail.com>
2023-10-24 17:53:19 +0530

    Commit-5

--- src/main/java/com/opencart/pages/HomePage.java
+++ src/main/java/com/opencart/pages/HomePage.java
@@ -1,9 +1,14 @@
 package com.opencart.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	
@@ -33,6 +38,8 @@ public class HomePage {
 	}
 	
 	public void clickOnMyAccount() {
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.visibilityOf(myAccountDropMenu));
 		myAccountDropMenu.click();
 	}
 	
